63bb41f6189d4dbf78f9629b559d7d1f53adb7f3
Matt Balmer <signup@mattbalmer.com>
2022-12-06 00:21:28 -0500

    day 6

---
+++ day-06/part1.ts
@@ -0,0 +1,30 @@
+import { Execute } from './format';
+
+const TRIGGER_LEN = 4;
+
+const containsRepeats = (chars: string[]): boolean => {
+  return Array.from(new Set(chars)).length !== chars.length;
+}
+
+export const execute: Execute = (line) => {
+  let window = [...line.slice(0, TRIGGER_LEN)];
+
+  if (!containsRepeats(window)) {
+    return TRIGGER_LEN + 1;
+  }
+
+  for(let i = TRIGGER_LEN; i < line.length; i++) {
+    const letter = line[i];
+
+    window = [
+      ...window.slice(1),
+      letter
+    ];
+
+    if (!containsRepeats(window)) {
+      return i + 1;
+    }
+  }
+
+  return -1;
+}
